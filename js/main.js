bf967d69cf60adafe19ffd53b8669abe44ec7898
Oscar Ekholm <i@oscarekholm.se>
2014-08-19 10:48:39 +0200

    Basic RequireJS main file

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * main
+ * The entry point for the RequireJS app, supposed to be
+ * including for example your Angular/Ember/whatever app
+ *
+ */
+
+// Define your main file's define name
+var entryPointName = 'main';
+
+require.config({
+    /**
+     * Put your RequireJS config here
+     */
+});
+
+define(entryPointName, [
+    /**
+     * Put your main dependencies here
+     */
+], function() {
+    'use strict';
+});
